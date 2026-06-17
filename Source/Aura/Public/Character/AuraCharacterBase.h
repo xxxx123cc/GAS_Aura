eedef8110fd84eb5eadd626abb473b95032c09e1
XN <1214463068@qq.com>
2026-06-17 16:15:38 +0800

    Character类的创建

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -18,12 +18,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Character|Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+	
 
 };
